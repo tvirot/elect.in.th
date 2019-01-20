80ddeb1349e0d1e982c354e512c2e3f121acc638
tvirot <tvirot@users.noreply.github.com>
2019-01-20 23:07:08 +0700

    added mini bar chart, implemented mouse over interaction

--- src/elect-social-listener.js
+++ src/elect-social-listener.js
@@ -7,8 +7,8 @@ const parties = [
     "อนาคตใหม่", 
     "ไทยรักษาชาติ", 
     "รวมพลังประชาชาติไทย", 
-    "เพื่อชาติ", 
-    "เพื่อธรรม", 
+    // "เพื่อชาติ", 
+    // "เพื่อธรรม", 
     "ประชาชาติ"
 ];
 
@@ -33,8 +33,8 @@ const colors = [
     '#d73027',
     '#f46d43',
     '#fdae61',
-    '#fee090',
-    '#ffffbf',
+    // '#fee090',
+    // '#ffffbf',
     '#e0f3f8',
     '#abd9e9',
     '#74add1',
@@ -42,11 +42,14 @@ const colors = [
     '#313695'
 ];
 
+const party2color = {};
+parties.forEach((d,i) => party2color[d] = colors[i]);
+
 /* ===== Streamgraph's Config ===== */ 
 const streamgraph = {
     width: 321, // Same fixed width for both desktop and mobile for simplicity
     side_margin: 30,
-    between_day_distance: 24, // 24px between days.
+    between_day_distance: 32, // 24px between days.
 
     svg: d3.select('#streamgraph svg'),
 
@@ -64,23 +67,35 @@ const streamgraph = {
 
 /* ===== Mini bar chart's config ===== */
 const minibar = {
-    width: 321, // Fixed width for simplicity
+    width: 500, // Fixed width for simplicity
+    margin: {
+        top: 32,
+        bottom: 48,
+        left: 160,
+        right: 30
+    },
     between_bar_distance: 24, // 8px between bars
+    n_top: 3,
 
     svg: d3.select('#minibar svg'),
 
-    x: d3.scaleLinear()
+    x: d3.scaleLinear(),
+
+    transition: d3.transition().duration(250)
 }
 
 const parseTime = d3.timeParse('%Y-%m-%d');
 const formatTime = d3.timeFormat('%a %b %d, %Y'); 
 
-d3.json('social-summary.json').then((raw) => init(raw));
+let raw;
+d3.json('social-summary.json').then((json) => {
+    raw = json;
+    // console.log(raw);
+    init(raw)
+});
 
 function init(raw) {
-    streamgraph.raw = raw;
-    // console.log(raw);
-
+    /*
     // Initialize FB / Twitter posts
     d3.select('#top-posts')
         .selectAll('.top-post')
@@ -100,6 +115,7 @@ function init(raw) {
                 `
             }
         });
+    */
 
     // Initialize streamgraph 
     let max_engagement = 0; 
@@ -116,11 +132,13 @@ function init(raw) {
     // console.log(data);
 
     const series = initStreamgraph(data); 
-
     renderStreamgraph(series); 
 
     // Initialize mini barchart
-    // initMinibar(max_engagement);
+    initMinibar(max_engagement);
+
+    // Pre-select first date
+    handleMouseover(raw[0]);
 }
 
 function initStreamgraph(data) {
@@ -151,26 +169,37 @@ function initStreamgraph(data) {
 }
 
 function renderStreamgraph(series) { 
-    streamgraph.g.selectAll('.highlightarea')
-        .data(streamgraph.raw)
-        .enter().append('rect')
-        .classed('highlightarea', true)
-        .attr('x', (d,i) => streamgraph.x(i) - streamgraph.between_day_distance / 2)
-        .attr('y', 0)
-        .attr('width', streamgraph.between_day_distance)
+    const highlights = streamgraph.g.selectAll('.highlight')
+        .data(raw)
+        .enter().append('g')
+        .attr('class', 'highlight hidden')
+        .attr('transform', (d,i) => `translate(${streamgraph.x(i) - streamgraph.between_day_distance / 2 - 2}, 0)`);
+        
+    highlights.append('rect')
+        .attr('class', 'highlight-area')
+        .attr('width', streamgraph.between_day_distance + 2)
         .attr('height', streamgraph.width);
-
+    
+    // highlights.append('line')
+    //     .attr('class', 'highlight-line')
+    //     .attr('y2', streamgraph.width);
+    // highlights.append('line')
+    //     .attr('class', 'highlight-line')
+    //     .attr('x1', (d,i) => streamgraph.between_day_distance)
+    //     .attr('x2', (d,i) => streamgraph.between_day_distance)
+    //     .attr('y2', streamgraph.width);
+    
     streamgraph.g.selectAll('path')
         .data(series)
         .enter().append('path')
         .attr('d', streamgraph.area)
-        .attr('fill', (d,i) => colors[i])
+        .attr('fill', d => party2color[d.key])
         // Give a smoother look at edges
         .attr('stroke', '#fbf8ed')
         .attr('stroke-width', 3)
         .attr('stroke-opacity', 0.25);
     
-    streamgraph.g.selectAll('line')
+    streamgraph.g.selectAll('.grid')
         .data(d3.range(series[0].length))
         .enter().append('line')
         .classed('grid', true)
@@ -181,7 +210,7 @@ function renderStreamgraph(series) {
         .attr('y2', (d,i) => ((i - 2) % 7 == 0) ? streamgraph.width : 35);
 
     streamgraph.g.selectAll('.hoverarea')
-        .data(streamgraph.raw)
+        .data(raw)
         .enter().append('rect')
         .classed('hoverarea', true)
         .attr('x', (d,i) => streamgraph.x(i) - streamgraph.between_day_distance / 2)
@@ -196,22 +225,85 @@ function renderStreamgraph(series) {
 function handleMouseover(d) {
     d3.select('#date')
         .text(formatTime(parseTime(d.created_date_bkk)));
-    d3.selectAll('.highlightarea')
-        .classed('highlight', dd => dd.created_date_bkk == d.created_date_bkk);
-    d3.selectAll('.top-post')
+    d3.selectAll('.highlight')
         .classed('hidden', dd => dd.created_date_bkk != d.created_date_bkk);
+    // d3.selectAll('.top-post')
+    //     .classed('hidden', dd => dd.created_date_bkk != d.created_date_bkk);
+    renderMinibar(d.stats.slice(0, minibar.n_top));
+    d3.select('#top-post')
+        .html(`[placeholder] - Top post of the day
+            <br/>${d.top_post.channel}
+            <br/>${d.top_post.user_name}
+            <br/>${d.top_post.text}
+            <br/>${d.top_post.permalink}
+        `);
 }
 
 function initMinibar(max_engagement) {
     minibar.svg
         .attr('width', minibar.width)
-        .attr('height', minibar.between_bar_distance * 3 + 12); // Showing top 3
-    minibar.g = minibar.svg.append('g'); 
+        .attr('height', minibar.between_bar_distance * minibar.n_top + minibar.margin.top + minibar.margin.bottom);
+    minibar.g = minibar.svg.append('g')
+        .attr('transform', `translate(${minibar.margin.left} ${minibar.margin.top})`); 
 
     minibar.x
         .domain([0, max_engagement])
-        .range([0, minibar.width]);
+        .range([0, minibar.width - minibar.margin.left - minibar.margin.right]);
+
+    minibar.xAxis = d3.axisBottom(minibar.x)
+        .ticks(5, 's')
+        .tickSizeInner(-minibar.between_bar_distance * minibar.n_top)
+        .tickSizeOuter(0)
+        .tickPadding(12);
+
+    minibar.bars = minibar.g.append('g');
+
+    minibar.g.append('g')
+        .attr('class', 'xaxis')
+        .attr('transform', `translate(0 ${minibar.between_bar_distance * (minibar.n_top - 0.5)})`)
+        .call(minibar.xAxis);
+
+    minibar.g.append('text')
+        .attr('class', 'bar-label-x')
+        .attr('x', (minibar.width - minibar.margin.left - minibar.margin.right) / 2)
+        .attr('y', minibar.between_bar_distance * minibar.n_top + 36)
+        .attr('text-anchor', 'middle')
+        .text('การมีส่วนร่วม');
     
-    // minibar.g.selectAll('bar')
-    //     .data()
+    // renderMinibar(raw[0].stats.slice(0, minibar.n_top));
+}
+
+function renderMinibar(top_three_stats) {
+    const bars = minibar.bars.selectAll('.bar')
+        .data(top_three_stats, (d,i) => i);
+
+    bars.exit().remove();
+    bars.enter().append('rect')
+        .attr('class', 'bar')
+        .attr('height', minibar.between_bar_distance * 0.6)
+        .attr('rx', 3)
+        .attr('ry', 3)
+        .attr('y', (d,i) => (i - 0.5) * minibar.between_bar_distance)
+        .attr('fill', d => party2color[d.party])
+        // .transition(minibar.transition)
+        .attr('width', (d,i) => minibar.x(d.total_engagement));
+    bars
+        .attr('y', (d,i) => (i - 0.5) * minibar.between_bar_distance)
+        .attr('fill', d => party2color[d.party])
+        // .transition(minibar.transition)
+        .attr('width', (d,i) => minibar.x(d.total_engagement));
+        
+    const labels = minibar.g.selectAll('.bar-label-y')
+        .data(top_three_stats, (d,i) => i);
+    
+    labels.exit().remove();
+    labels.enter().append('text')
+        .attr('class', 'bar-label-y')
+        .attr('x', -12)
+        .attr('text-anchor', 'end')
+        .text(d => d.party)
+        .attr('y', (d,i) => i * minibar.between_bar_distance);
+    labels
+        .text(d => d.party)
+        .attr('y', (d,i) => i * minibar.between_bar_distance);
 }
