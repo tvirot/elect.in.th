470bd7aae28a957d980f33fefa074c26a6238a4c
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-30 01:14:45 -0800

    localize date format

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # elect.in.th
 
 Visualizing Social Engagement
+
+For development
+
+```
+npm run start
+```
+
+To build final output
+
+```
+npm run build
+```
--- src/index.js
+++ src/index.js
@@ -33,6 +33,17 @@ const party2color = {};
 parties.forEach((d,i) => party2color[d] = colors[i]);
 console.log(party2color);
 
+const thaiLocale = d3.timeFormatLocale({
+  dateTime: "%A, %e %B %Y г. %X",
+  date: '%d %m %Y',
+  time: '%H:%M:%S',
+  periods: ["AM", "PM"],
+  days: ["วันอาทิตย์", "วันจันทร์", "วันอังคาร", "วันพุธ", "วันพฤหัส", "วันศุกร์", "วันเสาร์"],
+  shortDays: ["อา.", "จ.", "อ.", "พ.", "พฤ.", "ศ.", "ส."],
+  months: ["มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"],
+  shortMonths: ["ม.ค.", "ก.พ.", "มี.ค.", "เม.ย.", "พ.ค.", "มิ.ย.", "ก.ค.", "ส.ค.", "ก.ย.", "ต.ค.", "พ.ย.", "ธ.ค."]
+});
+
 /* ===== Streamgraph's Config ===== */
 const streamgraph = {
     width: 321, // Same fixed width for both desktop and mobile for simplicity
@@ -73,7 +84,7 @@ const minibar = {
 }
 
 const parseTime = d3.timeParse('%Y-%m-%d');
-const formatTime = d3.timeFormat('%a %b %d, %Y');
+const formatTime = t => `${thaiLocale.format('%a %_d %b')(t)} ${t.getFullYear()+543}`;
 
 let raw;
 d3.json(dataFile).then((json) => {
